dd1d2b65dfbe6b53e6b119c5eb1a95837bbc7664
Joshtri <stuffofyos1516@gmail.com>
2024-12-05 18:58:37 +0800

    adding data

---
+++ src/pages/Pengepul/PengepulPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Layout from '../Layout'
+import PengepulData from '../../components/Pengepul/PengepulData'
+
+export default function PengepulPage() {
+  return (
+        <Layout>
+            <PengepulData/>
+        </Layout>
+    )
+}
