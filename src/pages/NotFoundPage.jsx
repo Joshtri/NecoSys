f3ea860f0c64dae9e1fe533d22d3240b1c75a542
Joshtri <stuffofyos1516@gmail.com>
2024-12-06 08:46:33 +0800

    updating kembali btn

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,26 +1,30 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { Container, Button, Image } from 'react-bootstrap';
 
 const NotFoundPage = () => {
+  const navigate = useNavigate(); // Hook untuk navigasi
+
+  const handleGoBack = () => {
+    navigate(-1); // Kembali ke halaman sebelumnya
+  };
+
   return (
     <div className="d-flex flex-column justify-content-center align-items-center min-vh-100 bg-light">
       <Container className="text-center">
         <h1 className="display-1 fw-bold text-primary">404</h1>
         <p className="fs-4 text-secondary">Oops! Halaman yang Anda cari tidak ditemukan.</p>
         <Image
-          src="https://flowbite.com/docs/images/illustrations/404.svg"
+          src="https://bank-sampah-app-client.vercel.app/assets/3737258-Uin_m2QF.png"
           alt="Error illustration"
           fluid
           className="my-4"
           style={{ maxWidth: '400px' }}
         />
         <div>
-          <Link to="/">
-            <Button variant="primary" className="px-4 py-2">
-              Kembali ke Beranda
-            </Button>
-          </Link>
+          <Button variant="primary" className="px-4 py-2" onClick={handleGoBack}>
+            Kembali
+          </Button>
         </div>
       </Container>
     </div>
