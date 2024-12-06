b329250e3dbc1534f021958a8a02c0e35159d20c
Joshtri <stuffofyos1516@gmail.com>
2024-12-06 19:56:56 +0800

    updating

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+// import React from 'react';
 import PropTypes from 'prop-types';
 import { Navigate } from 'react-router-dom';
 import NotFoundPage from '../pages/NotFoundPage'; // Import halaman 404
