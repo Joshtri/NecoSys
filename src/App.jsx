7fd723fb7d3f5486e068e55a62d203cdd501b29e
Joshtri <stuffofyos1516@gmail.com>
2024-12-06 08:48:52 +0800

    updating

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,7 @@ import ViewTransaksiPage from './pages/Transaksi/ViewTransaksiPage'
 import PengepulPage from './pages/Pengepul/PengepulPage'
 import ViewPengepulPage from './pages/Pengepul/ViewPengepulPage'
 import NotFoundPage from './pages/NotFoundPage'
+import ProtectedRoute from './components/ProtectedRoute';
 
 function App() {
 
@@ -25,9 +26,55 @@ function App() {
           <Route path='/dashboard' element={<DashboardPage/>}/>
           <Route path='/' element={<Login/>}/>
 
-          <Route path="/admin/pengguna" element={<PenggunaPage />} />
-          <Route path="/admin/anggota" element={<AnggotaPage />} />
-          <Route path="/admin/kategori-sampah" element={<KategoriSampahPage />} />
+          {/* <Route path="/admin/pengguna" element={<PenggunaPage />} />
+          <Route path="/admin/anggota" element={<AnggotaPage />} /> */}
+
+           {
+            // ini bisa diakses utk pengepul dan admin, jadi endpoint awalan admin dihilangkna.
+           }
+
+                   {/* Rute yang hanya dapat diakses oleh admin */}
+        <Route
+          path="/admin/pengguna"
+          element={<ProtectedRoute element={PenggunaPage} allowedRoles={['admin']} />}
+        />
+        <Route
+          path="/admin/anggota"
+          element={<ProtectedRoute element={AnggotaPage} allowedRoles={['admin']} />}
+        />
+        <Route
+          path="/admin/pengepul"
+          element={<ProtectedRoute element={PengepulPage} allowedRoles={['admin']} />}
+        />
+        <Route
+          path="/admin/pengepul/:id"
+          element={<ProtectedRoute element={ViewPengepulPage} allowedRoles={['admin']} />}
+        />
+
+        {/* Rute yang dapat diakses oleh admin dan pengepul */}
+        <Route
+          path="/kategori-sampah"
+          element={<ProtectedRoute element={KategoriSampahPage} allowedRoles={['admin', 'pengepul']} />}
+        />
+        <Route
+          path="/item-sampah"
+          element={<ProtectedRoute element={ItemSampahPage} allowedRoles={['admin', 'pengepul']} />}
+        />
+        <Route
+          path="/transaksi"
+          element={<ProtectedRoute element={TransaksiPage} allowedRoles={['admin', 'pengepul']} />}
+        />
+        <Route
+          path="/transaksi/add"
+          element={<ProtectedRoute element={AddTransaksiPage} allowedRoles={['admin', 'pengepul']} />}
+        />
+        <Route
+          path="/transaksi/view/:id"
+          element={<ProtectedRoute element={ViewTransaksiPage} allowedRoles={['admin', 'pengepul']} />}
+        />
+
+
+          {/* <Route path="/admin/kategori-sampah" element={<KategoriSampahPage />} />
           <Route path="/admin/item-sampah" element={<ItemSampahPage />} />
           <Route path="/admin/transaksi" element={<TransaksiPage />} />
 
@@ -36,7 +83,7 @@ function App() {
           <Route path="/transaksi/view/:id" element={<ViewTransaksiPage />} />
 
           <Route path='/admin/pengepul' element={<PengepulPage/>}/>
-          <Route path="/admin/pengepul/:id" element={<ViewPengepulPage />} />
+          <Route path="/admin/pengepul/:id" element={<ViewPengepulPage />} /> */}
 
           {/* Rute wildcard untuk halaman 404 */}
           <Route path="*" element={<NotFoundPage />} />
