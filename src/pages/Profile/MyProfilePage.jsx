616a0e38bb33cf89429f6659537017e56caed4d4
Joshtri <stuffofyos1516@gmail.com>
2024-12-07 10:39:03 +0800

    addding package

---
+++ src/pages/Profile/MyProfilePage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Layout from '../Layout'
+import MyProfile from '../../components/MyProfile'
+
+export default function MyProfilePage() {
+  return (
+    <Layout>
+        <MyProfile/>
+    </Layout>
+  )
+}
